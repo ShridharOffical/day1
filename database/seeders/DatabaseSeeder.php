4b9e9c3d75aff235d446b9ff8bff136f4944d067
Shridhar Patil <shridharpatil826@gmail.com>
2024-01-18 15:20:14 +0530

    created fake data with in seddder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Student;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -24,22 +25,58 @@ class DatabaseSeeder extends Seeder
 
        $user= User::factory()->create();
 
-        Category::create([
+        $personal=Category::create([
             'name'=>'Personal',
             'slug'=>'personal'
         ]);
-        Category::create([
+        $family=Category::create([
             'name'=>'Family',
             'slug'=>'family'
         ]);
-        Category::create([
+        $work=Category::create([
             'name'=>'Work',
             'slug'=>'work'
         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Student::create([
+
+            'user_id'=>$user->id,
+            'category_id'=>$personal->id,
+            'title'=>'My Personal Post',
+            'slug'=>'my-personal-post',
+            'excerpt'=>'lorem personal doller si amet',
+            'body'=>'If you have manually added a column to a database table without using Laravel migrations and you want to reflect this change in your Laravel application, you have a few options:'
+            
+        ]);
+        Student::create([
+
+            'user_id'=>$user->id,
+            'category_id'=>$family->id,
+            'title'=>'My Family Post',
+            'slug'=>'my-family-post',
+            'excerpt'=>'lorem family doller si amet',
+            'body'=>'If you have manually added a column to a database table without using Laravel migrations and you want to reflect this change in your Laravel application, you have a few options:'
+            
+        ]);
+        Student::create([
+
+            'user_id'=>$user->id,
+            'category_id'=>$personal->id,
+            'title'=>'My Family Post',
+            'slug'=>'my-family-post',
+            'excerpt'=>'lorem family doller si amet',
+            'body'=>'If you have manually added a column to a database table without using Laravel migrations and you want to reflect this change in your Laravel application, you have a few options:'
+            
+        ]);
+        Student::create([
+
+            'user_id'=>$user->id,
+            'category_id'=>$work->id,
+            'title'=>'My Work Post',
+            'slug'=>'my-work-post',
+            'excerpt'=>'lorem work doller si amet',
+            'body'=>'If you have manually added a column to a database table without using Laravel migrations and you want to reflect this change in your Laravel application, you have a few options:'
+            
+        ]);
     }
 }
