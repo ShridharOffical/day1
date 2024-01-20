e5d6ec6ba0bf0b2dee0e1d8fd337a0a05f008769
Shridhar Patil <shridharpatil826@gmail.com>
2024-01-20 14:17:13 +0530

    corect position of svg buttun

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -13,12 +13,12 @@
 
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+        <div class="relative  lg:inline-flex  bg-gray-100 rounded-xl">
          
             <div x-data="{ show: false }"> 
                 <button
                  @click='show = !show' 
-                 class="py-2 pl-3 pr-9 text-sm font-semibold w-32 text-left inline-flex">
+                 class="py-2 pl-3  pr-9 text-sm font-semibold w-full lg:w-32 text-left inline-flex">
                  Categories
                  <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                  height="22" viewBox="0 0 22 22">
